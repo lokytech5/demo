9f5bb055c3ffe19947f52f9aec4ab9addcb60c24
lokytech5 <lokytech5@gmail.com>
2024-06-17 19:24:37 +0100

    Added colour update to navbar

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
                     <span className="badge badge-sm indicator-item">8</span>
                   </div>
                 </div>
-                <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-base-100 shadow">
+                <div tabIndex={0} className="mt-3 z-[1] card card-compact dropdown-content w-52 bg-primary-content shadow">
                   <div className="card-body">
                     <span className="font-bold text-lg">8 Items</span>
                     <span className="text-info">Subtotal: $999</span>
@@ -50,7 +50,7 @@ export default function Home() {
                     <img alt="Profile" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
                   </div>
                 </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-primary rounded-box w-52">
                   <li>
                     <a className="justify-between">
                       Profile
